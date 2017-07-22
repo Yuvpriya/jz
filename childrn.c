49bf816bc908d8bec58c478a786f336c59add3f7
Yuvpriya <Yuvpriya@users.noreply.github.com>
2017-07-22 11:58:36 +0530

    Create childrn.c

---
+++ childrn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int ch,x,p,i;
+printf("Enter the child");
+scanf("%d",&ch);
+printf("Pairs");
+scanf("%d",&p);
+for(i=0;i<2*ch;i++)
+{
+x=2*ch-i;
+if(p!=x)
+{
+printf("The pair is %d\n",p);
+}
+}
+}
